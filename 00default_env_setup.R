2626a3136c1b52567cda8dafa1454035e32438c9
Tom Purucker <purucker.tom@gmail.com>
2016-10-22 13:27:14 -0400

    env updates

--- 00default_env_setup.R
+++ 00default_env_setup.R
@@ -1,24 +1,41 @@
-#Install and load supporting libraries.
+#who am i
 print(Sys.info()[4])
 
+#r version installed
 R.Version()$version.string
+
+#flush
+all.objects <- objects()
+rm(list=all.objects)
+rm(all.objects)
+
+#base list of packages
+print("base packages: ")
+print((.packages()))
+
+#change options?
+#options(chmhelp=FALSE, htmlhelp=TRUE)
+
+#install support libraries
 library(rmarkdown, quietly = TRUE, warn.conflicts = FALSE)
 library(dplyr, quietly = TRUE, warn.conflicts = FALSE)
 library(knitr, quietly = TRUE, warn.conflicts = FALSE)
 library(ggplot2)
+
+#updated list of installed packages
 print("list of loaded packages: ")
 print((.packages()))
 
 #set some default directories based on machine location
 if(Sys.info()[4]=="DZ2626UTPURUCKE"){
-  dehyd.root <- "k:/git/glinski_dehydration/"
+  dir.root <- "k:/git/some_dir/"
 }
-print(paste("Root directory location: ", dehyd.root, sep=""))
+print(paste("Root directory location: ", dir.root, sep=""))
 
-dehyd.csv.in <- paste(dehyd.root, "csv_in/", sep="")
-dehyd.csv.out <- paste(dehyd.root, "csv_out/", sep="")
-dehyd.graphics <- paste(dehyd.root, "graphics/", sep="")
-dehyd.tables <- paste(dehyd.root, "tables/", sep="")
+dir.csv.in <- paste(dir.root, "csv_in/", sep="")
+dir.csv.out <- paste(dir.root, "csv_out/", sep="")
+dir.graphics <- paste(dir.root, "graphics/", sep="")
+dir.tables <- paste(dir.root, "tables/", sep="")
 
 #check to see if directories are accessible
-print(paste("check to see if R can access files OK: ", file.exists(dehyd.csv.in), sep = ""))
+print(paste("check to see if R can access files OK: ", file.exists(dir.csv.in), sep = ""))
